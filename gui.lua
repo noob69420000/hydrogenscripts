31e6751d45938a69d75733c5232bcbbb8823634f
noob69420000 <117397313+noob69420000@users.noreply.github.com>
2022-11-03 21:20:36 +0100

    Add files via upload

---
+++ gui.lua
@@ -0,0 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Dawidka Hub", "Sentinel")
+local Tab = Window:NewTab("Murder Mystery 2")
